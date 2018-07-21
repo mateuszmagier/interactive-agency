d69400c98e17f81839acb9874ee17a2aed09d259
mateuszmagier <mateuszmagier@bitbucket.org>
2018-07-21 11:49:57 +0200

    Fix stack order of middle project details; move arrows code to script.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     
+    function addNavArrows() {
+        let leftArrow = document.createElement("a");
+        leftArrow.classList.add("portfolio-section__arrow", "portfolio-section__arrow--left");
+        leftArrow.innerHTML = "<span>&#60;</span>";
+        
+        let rightArrow = document.createElement("a");
+        rightArrow.classList.add("portfolio-section__arrow", "portfolio-section__arrow--right");
+        rightArrow.innerHTML = "<span>&#62;</span>";
+        
+        let arrowsWrapper = document.createElement("div");
+        arrowsWrapper.classList.add("portfolio-section__arrow-wrapper");
+        arrowsWrapper.appendChild(leftArrow);
+        arrowsWrapper.appendChild(rightArrow);
+        
+        let arrowsContainer = document.createElement("div");
+        arrowsContainer.classList.add("portfolio-section__arrow-container");
+        arrowsContainer.appendChild(arrowsWrapper);
+        
+        let portfolioSection = document.querySelector(".portfolio-section");
+        portfolioSection.appendChild(arrowsContainer);
+    }
+    
+    addNavArrows();
+    
 });
