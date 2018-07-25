4b33eb3220dd8f3e38e74864faa7358267b03f80
mateuszmagier <mateuszmagier@bitbucket.org>
2018-07-25 13:27:17 +0200

    Add custom marker to map.

--- js/map.js
+++ js/map.js
@@ -5,11 +5,18 @@ function initMap() {
         lat: 40.71427,
         lng: -74.00597
     };
-    
-    let map = new google.maps.Map(document.getElementById('map'), {
+
+    let options = {
         center: newYork,
         zoom: 11
+    };
+
+    let map = new google.maps.Map(document.getElementById('map'), options);
+
+    let markerIcon = 'img/marker_small.png';
+    let marker = new google.maps.Marker({
+        position: newYork,
+        map: map,
+        icon: markerIcon
     });
-    
-    let marker = new google.maps.Marker({position: newYork, map: map});
 }
